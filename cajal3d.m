9b426a8941ba2e9136e26f7faa804c62ece69b2f
Alex Baden <alex.baden@gmail.com>
2015-06-12 17:32:00 -0400

    cajal 1.8.0 RC1 release

--- cajal3d.m
+++ cajal3d.m
@@ -65,7 +65,7 @@ classdef cajal3d
     end
     
     properties(Constant = true)
-        version = '1.8.0 (beta)';
+        version = '1.8.0 RC1';
         POC = 'ocp-support@googlegroups.com';
     end
     
